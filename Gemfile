0589b77b286ac1d9c8fe572a397a7de46a55f48e
Graham Hency <graham.hency@tangoe.com>
2019-02-27 14:45:13 -0500

    Added weather service

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby '2.4.4'
 
 gem 'sinatra'
+gem 'geocoder'
+gem 'httparty'
--- application.rb
+++ application.rb
@@ -1,5 +1,31 @@
+require 'json'
 require 'sinatra'
+require 'geocoder'
+require 'httparty'
 
 get '/' do
-  'Hello'
+  content_type :json
+
+  # {
+  #   coordinates: Geocoder.search('Indianapolis').first.coordinates
+  # }.to_json
+
+  response = HTTParty.get('https://api.darksky.net/forecast/32751df79f6c9f93897968c0d588dd69/37.8267,-122.4233')
+
+  {
+    weather: JSON.parse(response.body)
+  }.to_json
+end
+
+get '/forecast/:location' do
+  content_type :json
+  coordinates = Geocoder.search(params[:location]).first.coordinates
+
+  url = 'https://api.darksky.net/forecast/32751df79f6c9f93897968c0d588dd69/'
+
+  response = HTTParty.get(url + coordinates.first.to_s + ',' + coordinates.last.to_s)
+
+  {
+    weather: JSON.parse(response.body)
+  }.to_json
 end
